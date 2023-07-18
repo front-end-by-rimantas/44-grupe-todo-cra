67aa362b04d750f44be188a8205d85d253425b1a
Rimantas <belauzas@gmail.com>
2023-07-18 11:59:58 +0300

    Task list render

---
+++ src/components/main/Column.jsx
@@ -0,0 +1,24 @@
+import { BsThreeDots } from 'react-icons/bs';
+import style from './Main.module.css';
+
+export function Column({title, tasks}) {
+    const tasksList = [];
+    for (const id of tasks) {
+        tasksList.push(<div className={style.card}>TASK {id}</div>);
+    }
+    
+    return (
+        <div className={style.column}>
+            <div className={style.columnHeader}>
+                <p>{title} ({tasks.length})</p>
+                <BsThreeDots />
+            </div>
+            <div className={style.cardList}>
+                { tasks.map(id => <div className={style.card}>TASK {id}</div>) }
+            </div>
+            <div className={style.addTask}>
+                ADD TASK
+            </div>
+        </div>
+    );
+}
